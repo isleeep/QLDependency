87749c055ae08d1ccd43f67cb31cfd197a13cd77
isleeep <iruhua@gmail.com>
2023-01-31 17:00:47 +0800

    Update XinQLOneKey.sh

--- Shell/XinQLOneKey.sh
+++ Shell/XinQLOneKey.sh
@@ -50,7 +50,7 @@ pnpm add -g pnpm
 
 pnpm install -g
 
-npm install -g npm png-js date-fns axios crypto-js ts-md5 tslib @types/node requests tough-cookie jsdom download tunnel fs ws form-data
+npm install -g ds moment typescript npm png-js date-fns axios crypto-js ts-md5 tslib @types/node requests tough-cookie jsdom download tunnel fs ws form-data
 
 pnpm install -g js-base64 qrcode-terminal silly-datetime
 
